8624d00f35edbcf3e912db7a8a5c5844ab8fc939
Yordanis Tejeda <yordanisrodriguez855@gmail.com>
2023-04-20 12:19:14 -0700

    Arreglando los LinkedIterator
    Dándole solución al problema que tenían estas
    clases
    
    Estado: completado

--- Estructura_de_datos/src/Listas/Circular_Doble/LinkedIterator.java
+++ Estructura_de_datos/src/Listas/Circular_Doble/LinkedIterator.java
@@ -7,16 +7,20 @@ import java.util.Iterator;
  */
 public class LinkedIterator<E> implements Iterator<E> {
 
+    Circular_Doble<E> Lista;
     private Nodo<E> cursor;
+    int pos;
 
     public LinkedIterator(Circular_Doble<E> List) {
+        this.Lista = List;
         this.cursor = List.getFirst(); // Tener en cuenta si en la clase donde se va a utilizar tiene el get de getFirst()
+        pos = 0;
     }
 
     // Se sobreescriben de la clase Iterator original
     @Override
     public boolean hasNext() {
-        return cursor != null;
+        return pos < Lista.size();
     }
 
     @Override
@@ -24,6 +28,7 @@ public class LinkedIterator<E> implements Iterator<E> {
         if (hasNext()) {
             Nodo<E> aux = cursor;
             cursor = cursor.getNext();
+            pos++;
             return aux.getInfo();
         }
         return null;
--- Estructura_de_datos/src/Listas/Circular_Simple/LinkedIterator.java
+++ Estructura_de_datos/src/Listas/Circular_Simple/LinkedIterator.java
@@ -3,16 +3,21 @@ package Listas.Circular_Simple;
 import java.util.Iterator;
 
 public class LinkedIterator<E> implements Iterator<E> {
+
+    Circular_Simple<E> Lista;
     private Nodo<E> cursor;
+    int pos;
 
     public LinkedIterator(Circular_Simple<E> List) {
+        this.Lista = List;
         this.cursor = List.getFirst(); // Tener en cuenta si en la clase donde se va a utilizar tiene el get de getFirst()
+        pos = 0;
     }
 
     // Se sobreescriben de la clase Iterator original
     @Override
     public boolean hasNext() {
-        return cursor != null;
+        return pos < Lista.size();
     }
 
     @Override
@@ -20,6 +25,7 @@ public class LinkedIterator<E> implements Iterator<E> {
         if (hasNext()) {
             Nodo<E> aux = cursor;
             cursor = cursor.getNext();
+            pos++;
             return aux.getInfo();
         }
         return null;
