430381d24738c5c60649e4bd4415a8858c8d9a51
Yordanis Tejeda <yordanisrodriguez855@gmail.com>
2023-04-22 18:27:53 -0700

    Optimizando el código de las clases LinkedIterator

--- Estructura_de_datos/src/Listas/Circular_Doble/LinkedIterator.java
+++ Estructura_de_datos/src/Listas/Circular_Doble/LinkedIterator.java
@@ -7,20 +7,19 @@ import java.util.Iterator;
  */
 public class LinkedIterator<E> implements Iterator<E> {
 
-    Circular_Doble<E> Lista;
     private Nodo<E> cursor;
-    int pos;
+    private int pos, size;
 
     public LinkedIterator(Circular_Doble<E> List) {
-        this.Lista = List;
         this.cursor = List.getFirst(); // Tener en cuenta si en la clase donde se va a utilizar tiene el get de getFirst()
+        this.size = List.size();
         pos = 0;
     }
 
     // Se sobreescriben de la clase Iterator original
     @Override
     public boolean hasNext() {
-        return pos < Lista.size();
+        return pos < size;
     }
 
     @Override
